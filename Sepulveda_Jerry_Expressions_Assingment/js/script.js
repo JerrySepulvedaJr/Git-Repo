0fbce499818fb30dcd79f169350c33b652a9cdd0
Jerry Sepulveda Jr <j.sepulveda.jr@fullsail.edu>
2015-02-10 00:45:46 -1000

    Beginning js file added alert, var, prompt
    First portion of js file with alert, 3 var, and 1st prompt

--- Sepulveda_Jerry_Expressions_Assingment/js/script.js
+++ Sepulveda_Jerry_Expressions_Assingment/js/script.js
@@ -4,4 +4,14 @@
  * Expressions_Assignment
  */
 
+// I thought this would be a cool way of starting my DPS calculator, being that the user could click ok when ready to begin.
 
+alert("Are you ready to calculate the Damage per Second of your Weapon?");
+
+var Minimum Weapon Damage;
+
+var Maximum Weapon Daamge;
+
+var Weapon Speed;
+
+prompt Minimum Weapon Damage = ("Please enter the minimum damage of your weapon: ")
