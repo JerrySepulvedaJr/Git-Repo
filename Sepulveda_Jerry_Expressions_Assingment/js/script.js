8f85908cdb6ca5b9ecc750b75942ae39666dd932
Jerry Sepulveda Jr <j.sepulveda.jr@fullsail.edu>
2015-02-10 01:20:49 -1000

    console.logs set up and additional comments.

--- Sepulveda_Jerry_Expressions_Assingment/js/script.js
+++ Sepulveda_Jerry_Expressions_Assingment/js/script.js
@@ -8,15 +8,27 @@
 
 alert("Are you ready to calculate the Damage per Second of your Weapon?");
 
+// I'm setting up my variables for my DPS calculator here.
+
 var MinimumWeaponDamage;
 
 var MaximumWeaponDamage;
 
 var WeaponSpeed;
 
+// I'm setting up my prompts so I can get the users data.
+
 MinimumWeaponDamage = prompt("Please enter the minimum damage of your weapon: ");
 
 MaximumWeaponDamage = prompt("Please enter the maximum damage of your weapon: ");
 
 WeaponSpeed = prompt("Please enter the speed of your weapon: ");
+
+// I'm using the console.log to send the weapon data back to the user.
+
+console.log ("You entered the following minimum damage: "+ MinimumWeaponDamage + ".");
+
+console.log ("You entered the following maximum damage: "+ MaximumWeaponDamage + ".");
+
+console.log ("You entered the following weapon speed: "+ WeaponSpeed + ".");
 
